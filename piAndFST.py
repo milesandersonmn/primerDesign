bdc0d50c78d1e099c525eb2f5e162a8afd6a0b88
Miles Anderson <miles.anderson@tum.de>
2024-03-18 18:02:24 +0100

    Update piAndFST.py

--- piAndFST.py
+++ piAndFST.py
@@ -1,8 +1,9 @@
 import allel
 import numpy as np
 import pandas
+import csv
 samples = ["LA2750_N02", "LA2749_NO2", "LA2932_N02", "LA4109_NO1", "LA4108_T30", "LA4336_NO1", "LA4107_N01", "LA4106_NO2", "LA4338_NO2", "LA4339_NO1", "LA2884_NO5", "LA4329_NO2", "LA4330_N02", "LA4332_X1", "LA4117A_N05", "LA4118_N02", "LA4119_V16", "LA4334_NO1", "LA4335_NO1", "LA2880_N17"]
-samples[0:10]
+
 with open('all_genes.bed') as bedFile:
 	for gene in bedFile:
 		try:
@@ -11,13 +12,11 @@ with open('all_genes.bed') as bedFile:
 			Start = int(region[1]) + 1
 			End = int(region[2]) + 1
 			Coordinate = Chr + ":" + str(Start) + "-" + str(End)
-			print(Coordinate) 
 			callset = allel.read_vcf('Schil_capture_variants_AllChroms.decomposedVariants.filtered.maxMissing.sorted.reheader.vcf.gz', fields='*', region=Coordinate)
 			pos = allel.SortedIndex(callset['variants/POS'])
 			gt_array = allel.GenotypeArray(callset['calldata/GT'])
 			alleleCountArray = gt_array.count_alleles()
 			pi = allel.sequence_diversity(pos, alleleCountArray)
-			pi
 			callset = allel.read_vcf('Schil_capture_variants_AllChroms.decomposedVariants.filtered.maxMissing.sorted.reheader.vcf.gz', fields='*', region=Coordinate, samples=samples)
 			pos = allel.SortedIndex(callset['variants/POS'])
 			gt_array = allel.GenotypeArray(callset['calldata/GT'])
@@ -26,6 +25,14 @@ with open('all_genes.bed') as bedFile:
 			acHighland = gt_array.count_alleles(subpop=subpops[1])
 			num, den = allel.hudson_fst(acCoast, acHighland)
 			fst = np.sum(num) / np.sum(den)
+			Coordinate
+			pi
 			fst
+			with open('piAndFST.csv', 'a', newline='') as csvfile:
+				writer = csv.writer(csvfile, delimiter='\t')
+				row = [Chr,Start,End,pi,fst]
+				print(row)
+				writer.writerow(row)
+						
 		except:
 			pass
